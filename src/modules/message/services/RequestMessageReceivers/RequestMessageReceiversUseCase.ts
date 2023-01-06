356a259a91ea55cc42c42cc21b4e428064878bdf
Matheus Guirra Sousa ✝️ <guirramatheus1@gmail.com>
2023-01-06 20:33:23 +0000

    fix: atualizando requestService para atualizar mensagens a serem disparadas

--- src/modules/message/services/RequestMessageReceivers/RequestMessageReceiversUseCase.ts
+++ src/modules/message/services/RequestMessageReceivers/RequestMessageReceiversUseCase.ts
@@ -2,20 +2,23 @@ import { IDateProvider } from "../../../../shared/providers/dateProvider/IDatePr
 import { ISubRepository } from "../../../sub/infra/prisma/repositories/ISubRepository";
 import { IMessageRepository } from "../../infra/prisma/repositories/IMessageRepository";
 import { MessagesFlowUseCase } from "../Messages/MessagesFlowUseCase";
+import { IUpdateSubLastMessageDTO } from "../../../sub/dtos/IUpdateSubLastMessage";
 
-interface ITransportMessageReceiver {
+export interface ITransportMessageReceiver {
     receiver: {
         email: string,
         actualCase: string,
     },
     msg: {
         msgCase: string,
-        msgs: [{
-            id: string,
-            msgDescription: string,
-            position: number
-        }],       
+        msgs: IMsgs[],       
     }
+}
+
+interface IMsgs {
+    id: string,
+    msgDescription: string,
+    position: number
 }
 
 export class RequestMessageReceiversUseCase {
@@ -30,7 +33,7 @@ export class RequestMessageReceiversUseCase {
         const subs = await this.subRepository.findAll();
         let receivers: string[] = [];
 
-        let transportMessageReceivers: ITransportMessageReceiver[];
+        let transportMessageReceivers: ITransportMessageReceiver[] = [];
 
         subs.map(sub => {
            const getMessageToSend = async () => {
@@ -43,30 +46,76 @@ export class RequestMessageReceiversUseCase {
                 }
             }
 
-            const [msgToSend] = await Promise.all(
-                [
-                    await this.messageRepository.findAll()
-                        .then(msgs => msgs.filter(msg => msg.props.id !== sub.props.last_message.id 
-                        && msg.props.msgCases === sub.props.actualCase)),       
-                ]);
-
-                transportMessageReceivers.push({
-                    receiver: {
-                        email: sub.props.email,
-                        actualCase: sub.props.actualCase
-                    },
-                    msg: {
-                        msgCase: sub.props.actualCase,
-                        msgs: msgToSend,
+            const checkIsUndefined = async () => {
+                if(!sub.props.last_message && sub.id){
+                    const [msgsInSubCase, dateNow] = await Promise.all([
+                        await this.messageRepository.findAll()
+                        .then(
+                            msgs => msgs.filter(msg => msg.props.msgCases === sub.props.actualCase)
+                            ),
+                        await this.dateProvider.dateNow()
+                    ]);
+    
+                    const msgsInSubCaseLength = msgsInSubCase.length - 1;
+                    const getFirstMessageInFlow = msgsInSubCase[msgsInSubCaseLength];
+    
+                    const request: IUpdateSubLastMessageDTO = {
+                        id: getFirstMessageInFlow.props.id,
+                        msgCases: getFirstMessageInFlow.props.msgCases,
+                        msgDescription: getFirstMessageInFlow.props.description,
+                        subId: sub.id,
+                        send_at: dateNow
                     }
-                })
+    
+                    sub.props.last_message = request;
+                    return sub.props.last_message.id;
+                } else if (sub.props.last_message && sub.id){
+                    return sub.props.last_message.id;
+                }
             }
 
+            const collectMsgsAndSend = async () => {
+                const [msgToSend] = await Promise.all(
+                    [
+                        await this.messageRepository.findAll()
+                            .then(msgs => msgs.filter(async (msg) => msg.props.id !== await checkIsUndefined()
+                            && msg.props.msgCases === sub.props.actualCase)),       
+                    ]);
+    
+                    
+                    let overlapMsgsProps: IMsgs[] = [];
+
+                    msgToSend.map(toSend => {
+                       const mappedToSendMsgProps = async () => {
+                        overlapMsgsProps.push({
+                            id: toSend.props.id,
+                            msgDescription: toSend.props.description,
+                            position: msgToSend.findIndex(msg => msg.props === toSend.props)
+                        });
+                       }
+
+                       mappedToSendMsgProps();
+                    });
+
+                    transportMessageReceivers.push({
+                        receiver: {
+                            email: sub.props.email,
+                            actualCase: sub.props.actualCase
+                        },
+                        msg: {
+                            msgCase: sub.props.actualCase,
+                            msgs: overlapMsgsProps,
+                        }
+                    });
+            }  
             
+            collectMsgsAndSend();
+            }
+
             getMessageToSend();
         });
 
 
-        await this.messagesFlow.execute(receivers);
+        await this.messagesFlow.execute(transportMessageReceivers);
     }
 }
